3e0f6bc6e08866171a9c0bd1ec441647181eec3b
MYXHub <108988482+MyxRojav@users.noreply.github.com>
2026-08-22 18:43:44 +0300

    Update documentation.lua

--- documentation.lua
+++ documentation.lua
@@ -1,16 +1,19 @@
+local CustomUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MyxRojav/ThermalUI/refs/heads/main/ThermalUI.lua"))()
+
 local window = CustomUI:CreateWindow({
     LogoID = "rbxassetid://137394765830675",
     TopText = "MYX Arsenal v2.0",
     UIKeybind = "RightControl"
 })
 
+local mainTab = CustomUI:CreateTab(window, { Name = "Main" })
 local settingsTab = CustomUI:CreateTab(window, { Name = "Settings" })
 
 CustomUI:CreateKeybind(settingsTab, {
     Name = "UI Toggle Keybind",
     DefaultKey = "RightControl",
     Callback = function(key)
-        window:SetKeybind(key)  -- updates the UI toggle key
+        window:SetKeybind(key)
         print("UI keybind set to:", key)
     end
 }, 1)
